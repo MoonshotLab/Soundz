c1014891bb1d9f683a4e967da3257d29ef853dba
Eric Goeken <EGoeken@barkleyus.com>
2015-02-26 10:59:30 -0600

    adding sound loop

--- server.js
+++ server.js
@@ -5,23 +5,67 @@ var path = require('path');
 var port = process.env.PORT || 3000;
 var app = express();
 var arduino = require('./lib/arduino');
+var soundLoop = require('./lib/soundLoop');
 
+
+
+var pins = [
+	11,
+	0,
+	10,
+	8,
+	0,
+	0,
+	3,
+	11,
+	0,
+	10,
+	8,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+];
+
+
+
+//	Web
+//
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.get('/', function(req ,res){
+	res.render('index');
+	soundLoop.start(200,300);
+});
 var server = http.Server(app);
 server.listen(port, function(){
   console.log('server started, listening on port ' + port + '...');
 });
 
 
-app.get('/', function(req ,res){
-  res.render('index');
+//	Socket -> Out
+//
+var io = require('socket.io')(server);
+io.on('connection', function(socket){
+	console.log('connected');
+  	pins.forEach(function(val,i){
+  		console.log("update display:"+i+" "+val);
+		io.emit('update',{index:i,position:val});
+	});
 });
 
 
-var io = require('socket.io')(server);
-io.on('connection', function(socket){
-  console.log('connected');
+// Arduino -> In
+//
+var TIME_SLOTS = 15;
+var reset = function(){
+	pins = Array.apply(null, new Array(15)).map(Number.prototype.valueOf,0);
+}
+arduino.events().on('pinChange',function(pinIdx){
+	console.log("pin:"+pinIdx);
+	var pin = pins[pinIdx] = pins[pinIdx]+1;
+	//soundLoop.updateNote(pinIdx, pin);
 });
