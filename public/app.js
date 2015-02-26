6cefd83859dc8c7fcfa02e6baa621c141fc9eceb
Joe Longstreet <joelongstreet@gmail.com>
2015-02-26 09:43:05 -0600

    ui updates

--- public/app.js
+++ public/app.js
@@ -1,2 +1,20 @@
 var socket = io();
-console.log('hello there');
+
+// there's always exactly twelve notes
+// { position : 0 to 12, index : 0 to however many ballz }
+socket.on('update', function(opts){
+  var $ball = $('.stripe-' + opts.index);
+  $ball.css({
+    top : opts.position/12 * 100 + '%'
+  });
+});
+
+
+// { index : 0 to however many ballz }
+socket.on('beat', function(opts){
+  var $ball = $('.stripe-' + opts.index);
+  $ball.addClass('beat');
+  setTimeout(function(){
+    $ball.removeClass('beat');
+  }, 500);
+});
